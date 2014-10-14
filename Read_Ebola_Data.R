52d14ace017f660d5ae2807192162dadcae3aac6
muschellij2 <muschellij2@gmail.com>
2014-10-14 12:52:45 -0400

    Fixed cases

--- Read_Ebola_Data.R
+++ Read_Ebola_Data.R
@@ -1,5 +1,7 @@
 rm(list=ls())
 library(XML)
+library(stringr)
+library(plyr)
 ###########################
 # West Africa Data
 ###########################
@@ -12,13 +14,53 @@ afr.tab = readHTMLTable(doc)
 ###########################
 drc_url = "http://www.cdc.gov/vhf/ebola/outbreaks/drc/2014-august.html"
 ddoc = htmlParse(drc_url, isURL = TRUE)
-dat = xpathSApply(ddoc, "//div[@class='module-typeA']/p", xmlValue)
-dat = paste(dat, collapse = " ")
-cases = gsub("(.*) (.*) cases(.*)", "\\2", dat)
-deaths = gsub("(.*) (.*) deaths(.*)", "\\2", dat)
-cases = as.numeric(cases)
-deaths = as.numeric(deaths)
+#### current data
+cdat = xpathSApply(ddoc, "//div[@class='module-typeA']/p", xmlValue)
+cdat = cdat[!grepl("^For information about(.*) web page[.]$", cdat)]
+#### previous data
+dat = xpathSApply(ddoc, "//div[@class='rx-row']/p", xmlValue)
+ind = grepl("^For information about(.*) web page[.]$", dat)
+group = cumsum(ind) + 1
+group = group[!grepl("^For information about(.*) web page[.]$", dat)]
+dat = dat[!grepl("^For information about(.*) web page[.]$", dat)]
 
+### last 2 are separated
+ldat = length(dat)
+dat[length(dat)]
+dat = c(cdat, dat)
+group = c(0, group)
+
+data = data.frame(group=group, dat= dat, stringsAsFactors=FALSE)
+dat = daply(data, .(group), function(x){
+	paste0(x$dat, collapse = " ")
+})
+names(dat) = NULL
+##############################
+# Parse strings of death counts over time
+##############################
+month.string = paste0('(', paste0(month.name, collapse="|"), ")")
+str1 = paste0("((As of|On) ", month.string, " (\\d\\d|\\d), 201\\d), the.*")
+dates = gsub(str1, "\\1", dat)
+dates = gsub("^(As of|On) ", "", dates)
+
+dat = gsub("suspected cases", "cases", dat)
+dat = gsub("total number of cases to (\\d{1,})", "\\1 cases", dat)
+
+cases = sub("(.*) (\\d|\\d\\d) cases(.*)", "\\2", dat)
+cases = as.numeric(cases)
+
+dat = gsub("number of deaths reported to (\\d{1,})", 
+	"\\1 deaths", dat)
+
+deaths = gsub("(.*) (.*) deaths(.*)", "\\2", dat)
+deaths = as.numeric(deaths)
+hist.data = data.frame(date=as.Date(dates, format = "%B %d, %Y"), 
+	cases = cases,
+	deaths = deaths)
+
+###################################
+# Extra tab
+###################################
 cdc_chart = "http://www.cdc.gov/vhf/ebola/outbreaks/history/distribution-map.html"
 cdoc = htmlParse(cdc_chart, isURL = TRUE)
 cdc.tab = readHTMLTable(cdoc, 
